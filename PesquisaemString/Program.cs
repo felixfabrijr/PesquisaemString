de346464431fbf956147bb82ddf4afdfd09f3f0b
felixfabri <felix.fabrijr@gmail.com>
2023-02-06 14:15:21 +0000

    Add project files.

---
+++ PesquisaemString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string letraMusica = "you said goodbye, and I say hello!";
+        Console.WriteLine(letraMusica.Contains("goodbye"));
+        Console.WriteLine(letraMusica.Contains("love)"));
+
+        Console.WriteLine(letraMusica.StartsWith("you"));
+        Console.WriteLine(letraMusica.StartsWith("goodbye"));
+
+    }
+}

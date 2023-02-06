3d068344a677efc5d739b3c14f3a4212c2971c18
felixfabri <felix.fabrijr@gmail.com>
2023-02-06 14:22:27 +0000

    Pesquisa em string

--- PesquisaemString/Program.cs
+++ PesquisaemString/Program.cs
@@ -5,11 +5,25 @@ public class Program
     public static void Main()
     {
         string letraMusica = "you said goodbye, and I say hello!";
-        Console.WriteLine(letraMusica.Contains("goodbye"));
-        Console.WriteLine(letraMusica.Contains("love)"));
+        //Console.WriteLine(letraMusica.Contains("goodbye"));
+        //Console.WriteLine(letraMusica.Contains("love)"));
 
-        Console.WriteLine(letraMusica.StartsWith("you"));
-        Console.WriteLine(letraMusica.StartsWith("goodbye"));
+        //Console.WriteLine(letraMusica.StartsWith("you"));
+        //Console.WriteLine(letraMusica.StartsWith("goodbye"));
+
+        //Console.WriteLine(letraMusica.Contains("goodbye"));
+        //Console.WriteLine(letraMusica.Contains("love"));
+
+        //Console.WriteLine(letraMusica.StartsWith("love"));
+        //Console.WriteLine(letraMusica.StartsWith("you"));
+
+        //Console.WriteLine(letraMusica.EndsWith("say"));
+        //Console.WriteLine(letraMusica.EndsWith("you"));
+
+        bool comecaComVoce = letraMusica.StartsWith("you");
+        Console.WriteLine(comecaComVoce);
+
+
 
     }
 }
